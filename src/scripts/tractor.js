01c8feb0762cd4f72d50c3728a0d3bf3d4fbfa16
Travis Stevenson <thetechtravis@outlook.com>
2020-10-13 20:05:01 -0500

    Created tractor.js file and commented in tasks to do.

---
+++ src/scripts/tractor.js
@@ -0,0 +1,8 @@
+// Accepts the year's planting plan as input.
+// The plan is an array. It contains 4 arrays representing 
+// the rows in the field to be plants. Figure out how to iterate 
+// both the parent array and the child arrays.
+
+export const plantSeeds = (yearlyPlan) => {
+    
+}
